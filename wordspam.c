4f115047b9df781c8eaa28ba26b231d83cdfd60b
KurtG4ry <kurtgary4@gmail.com>
2026-07-08 22:24:45 +0200

    Added the ability to type in the color word instead of a number
    pretty much it.

--- wordspam.c
+++ wordspam.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <string.h>
+#include <strings.h>
 
   int main(){
      char word[80] = "";
      char response = '\0';
-     int choice = 0;
+     char choice[10] = "";
 
      printf("Enter word: ");
      fgets(word, sizeof word, stdin);
      word[strlen(word) - 1] = '\0';
 
-     printf("What color do you want: \n");
+     printf("What color do you want(type the name of the color lowercased): \n");
        printf("[1] GREEN\n");
        printf("[2] RED\n");
        printf("[3] YELLOW\n");
@@ -18,55 +19,46 @@
        printf("[5] MAGENTA\n");
        printf("[6] CYAN\n");
     printf("Enter choice here: \n");
-    scanf("%d", &choice);
+    fgets(choice, sizeof choice, stdin);
+     choice[strlen(choice) - 1] = '\0';
 
-     printf("Are you ready?(Y/N): \n");
-     scanf(" %c", &response);
+    if(strstr(choice, "green") != NULL) {
 
-     if(response != 'Y' && response != 'y'){
-
-        printf("Why did you even run this program then :sob:\n");
-
-     } else {
-
-    switch(choice) {
-
-        case 1:
-            //green
             while(1 == 1) {
                 printf("\033[0;32m%s\033[0m", word);
             }
-
-        case 2:
-            //red
+        }
+    if(strstr(choice, "red") != NULL){
+            //green
             while(1 == 1) {
                 printf("\033[0;31m%s\033[0m", word);
             }
-
-        case 3:
+        }
+        if(strstr(choice, "yellow") != NULL){
             // yellow
             while(1 == 1) {
                 printf("\033[0;33m%s\033[0m", word);
             } //closes while function
-
-         case 4:
+        }
+         if(strstr(choice, "blue") != NULL){
             // blue
             while(1 == 1) {
                 printf("\033[0;34m%s\033[0m", word);
             } //closes while function   
-
-            case 5:
+        }
+         if(strstr(choice, "magenta") != NULL){
             // magenta
             while(1 == 1) {
                 printf("\033[0;35m%s\033[0m", word);
             } //closes while function  
-             
-            case 6:
+        }
+         if(strstr(choice, "cyan") != NULL){
             // cyan
             while(1 == 1) {
                 printf("\033[0;36m%s\033[0m", word);
             } //closes while function  
-        } // close statement
-} //close else block
+         }else{
+            printf("Unknown color please restart the program and type the colors listed\n");
+         }
     return 0;
   }
